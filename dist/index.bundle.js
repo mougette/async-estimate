9fe7963d559dd6b20fb02def5557098da59a02f5
Noah Stewart <nstewart@cihedging.com>
2024-06-21 23:11:09 -0500

    Added bones

---
+++ dist/index.bundle.js
@@ -0,0 +1,1 @@
+var e={d:(o,t)=>{for(var r in t)e.o(t,r)&&!e.o(o,r)&&Object.defineProperty(o,r,{enumerable:!0,get:t[r]})},o:(e,o)=>Object.prototype.hasOwnProperty.call(e,o)},o={};e.d(o,{t:()=>t});const t=()=>{console.log("hello world")};var r=o.t;export{r as test};
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path')
+
+module.exports = {
+    entry: {
+        index: "./src/index.ts",
+    },
+    mode: "production",
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: [
+                    {
+                        loader: "ts-loader",
+                        options: {
+                            compilerOptions: { noEmit: false },
+                        },
+                    },
+                ],
+                exclude: /node_modules/,
+            },
+            {
+                exclude: /node_modules/,
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".js", "jsx", ".ts", ".tsx"],
+    },
+    output: {
+        libraryTarget: "module",
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    experiments: {
+        outputModule: true
+    },
+}
